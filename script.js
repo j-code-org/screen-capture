809dcc5e3b473a71934dfe176349b512558bd528
Koichi Ito <koichii@live.jp>
2017-08-28 05:27:54 +0900

    update

--- script.js
+++ script.js
@@ -1,30 +1,122 @@
 const puppeteer = require('puppeteer');
 const domain = 'https://studio.code.org';
-var course = '/s/course3';
-var stage = 2;
-var puzzle = 3;
 const imagepath = './img/';
+
+async function oneScreenshot( page, c, s, p, ex ) {
+
+  const course = '/s/course';
+  const stage = 2;
+  const puzzle = 3;
+  
+  let pagename =  domain + '/s/course' + c + '/stage/' + s + '/puzzle/' + p;
+  let filename =  imagepath + c + '/c' + c + 's' + zero2( s ) + 'p' + zero2( p ) + ex + '.png';
+  console.log( pagename, filename );
+  try {
+    await page.goto( pagename, {
+      waitUntil: 'networkidle'
+    });
+  } catch ( err ) {
+    console.log( "page.goto error !" );
+  }
+  return page.screenshot({ path: filename });
+}
+
+function zero2( n ) {
+  return ('00' + n).slice( -2 );
+}
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto( domain + course + '/stage/' + stage + '/puzzle/' + puzzle, {
-	waitUntil: 'networkidle'
+  await page.setViewport({ width: 1280, height: 720 });
+  await page.goto( domain + '/course', {
+  	waitUntil: 'networkidle'
   });
   console.log( await page.cookies() );
-  await page.screenshot({path: imagepath + 'example.png' });
+  await page.screenshot({ path: imagepath + 'example.png' });
+  // 日本語に設定 
+  await page.setCookie({ name:'language_', value:'ja' } );
 
-  await page.setCookie( { name:'language_', value:'ja' } );
-  stage = 2;
-  puzzle = 5;
-
-  for ( var p = 1; p < 15; p++ ) {
-    console.log( 'get:' + 's' + stage + 'p' + p );
-    await page.goto( domain + course + '/stage/' + stage + '/puzzle/' + p, {
-        waitUntil: 'networkidle'
-    });
-    //console.log( await page.cookies() );
-    await page.screenshot({path: imagepath + 's' + stage + 'p' + p + '.png' });
+  const courses = [
+    {course: 2, section: [
+      3,
+      3,
+      11,
+      12,
+      2,
+      14,
+      16,
+      14,
+      3,
+      11,
+      12,
+      2,
+      15,
+      2,
+      2,
+      10,
+      11,
+      2,
+      13
+    ]},
+    {course: 3, section: [
+      3,
+      15,
+      15,
+      3,
+      10,
+      11,
+      10,
+      12,
+      2,
+      3,
+      12,
+      9,
+      13,
+      13,
+      10,
+      6,
+      7,
+      3,
+      3,
+      2,
+      15
+    ]},
+    {course: 4, section: [
+      1,
+      9,
+      14,
+      1,
+      1,
+      16,
+      8,
+      1,
+      11,
+      12,
+      7,
+      13,
+      1,
+      17,
+      11,
+      8,
+      1,
+      11,
+      6,
+      8,
+      5,
+      5
+    ]}
+  ];
+  const c = courses[2].course;
+  const tbl = courses[2].section;
+  
+  for ( let s = 0; s < tbl.length; s++ ) {
+    console.log( "course3 ", s+1, tbl[s] );
+    for ( let p = 0; p < tbl[s]; p++ ) {
+      await oneScreenshot(page, c, s+1, p+1, "a");
+      await page.mouse.click(100, 100);
+      await oneScreenshot(page, c, s+1, p+1, "b");
+    }
   }
 
   browser.close();
